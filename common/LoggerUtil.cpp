77610977a1f42cdea61767874076e78e1e63597f
Jeremy Xu <jeremyxu2010@gmail.com>
2013-05-29 15:09:46 +0800

    initial commit

---
+++ common/LoggerUtil.cpp
@@ -0,0 +1,18 @@
+/*
+ * LoggerUtil.cpp
+ *
+ *  Created on: 2013-5-29
+ *      Author: jeremy
+ */
+
+#include "LoggerUtil.hpp"
+
+LoggerUtil::LoggerUtil() {
+	// TODO Auto-generated constructor stub
+
+}
+
+LoggerUtil::~LoggerUtil() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ common/LoggerUtil.hpp
@@ -0,0 +1,17 @@
+/*
+ * LoggerUtil.hpp
+ *
+ *  Created on: 2013-5-29
+ *      Author: jeremy
+ */
+
+#ifndef LOGGERUTIL_HPP_
+#define LOGGERUTIL_HPP_
+
+class LoggerUtil {
+public:
+	LoggerUtil();
+	virtual ~LoggerUtil();
+};
+
+#endif /* LOGGERUTIL_HPP_ */
